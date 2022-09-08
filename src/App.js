fa7386e4e21a00b08eb42104d4953104c2c88db9
Igor Beduin <beduinigor@gmail.com>
2022-09-08 01:41:53 -0300

    feat: adds ToastContainer and initialize app database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 import { AuthenticationContextProvider } from "./contexts/AuthenticationContext";
 import { RoutesNavigator } from "./navigation/RoutesNavigator";
 
+import { useInitialConfiguration } from "hooks/useInitialConfiguration";
+
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
+  useInitialConfiguration();
+
   return (
     <BrowserRouter>
+      <ToastContainer />
       <AuthenticationContextProvider>
         <RoutesNavigator />
       </AuthenticationContextProvider>

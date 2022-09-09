d50d558ad18408310211db243534fc84d35ba72b
Igor Beduin <beduinigor@gmail.com>
2022-09-09 00:22:57 -0300

    feat: render Sidebar component and react router outlet on nav and main content areas

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useAuthenticationContext } from "contexts/AuthenticationContext";
+import { Outlet } from "react-router-dom";
 
+import { Sidebar } from "components/Sidebar";
 
 export function Dashboard() {
   return (
@@ -8,13 +9,12 @@ export function Dashboard() {
       <div className="bg-slate-50 h-screen flex flex-row justify-start items-center">
         <nav className="w-80 h-full flex justify-center items-center p-2">
           <div className="bg-slate-100 h-full w-full rounded-lg">
-            <button onClick={() => setIsSignedIn(false)}>Logout</button>
+            <Sidebar />
           </div>
         </nav>
         <main className="w-full h-screen flex justify-center items-center p-2">
           <div className="bg-slate-100 h-full w-full rounded-lg">
-        <main className="w-full h-screen flex justify-center items-center">
-          <div className="bg-green-200 h-5/6 w-11/12 rounded-lg"></div>
+            <Outlet />
           </div>
         </main>
       </div>

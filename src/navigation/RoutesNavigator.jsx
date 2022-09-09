044f9106b0aae8141062c125aa30139513519900
Igor Beduin <beduinigor@gmail.com>
2022-09-09 00:10:57 -0300

    feat: adds navigation to nested private routes

--- src/navigation/RoutesNavigator.jsx
+++ src/navigation/RoutesNavigator.jsx
@@ -3,6 +3,9 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import { useAuthenticationContext } from "contexts/AuthenticationContext";
 import { Dashboard, SignUp, Login } from "pages";
+import { Home } from "pages/Home";
+import { Negotiate } from "pages/Negotiate";
+import { Transactions } from "pages/Transactions";
 
 export function RoutesNavigator() {
   const { isSignedIn } = useAuthenticationContext();
@@ -12,12 +15,16 @@ export function RoutesNavigator() {
       <Routes>
         {isSignedIn ? (
           <>
-            <Route path="*" element={<Navigate to="/" />} />
-            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+            <Route path="/" element={<Dashboard />}>
+              <Route path="home" element={<Home />} />
+              <Route path="negotiate" element={<Negotiate />} />
+              <Route path="transactions" element={<Transactions />} />
+            </Route>
           </>
         ) : (
           <>
-            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="*" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
           </>

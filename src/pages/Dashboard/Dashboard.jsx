02737e4855761fb763e0306dc1a1c9687dd1fe34
Igor Beduin <beduinigor@gmail.com>
2022-09-09 00:22:22 -0300

    refact: update nav and main content positioning and size

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { useAuthenticationContext } from "contexts/AuthenticationContext";
 
-export function Dashboard() {
-  const { setIsSignedIn } = useAuthenticationContext();
 
+export function Dashboard() {
   return (
     <>
       <div className="bg-slate-50 h-screen flex flex-row justify-start items-center">
-        <nav className="w-80 h-full flex justify-center items-center">
-          <div className="bg-red-200 h-5/6 w-11/12 rounded-lg">
+        <nav className="w-80 h-full flex justify-center items-center p-2">
+          <div className="bg-slate-100 h-full w-full rounded-lg">
             <button onClick={() => setIsSignedIn(false)}>Logout</button>
           </div>
         </nav>
+        <main className="w-full h-screen flex justify-center items-center p-2">
+          <div className="bg-slate-100 h-full w-full rounded-lg">
         <main className="w-full h-screen flex justify-center items-center">
           <div className="bg-green-200 h-5/6 w-11/12 rounded-lg"></div>
+          </div>
         </main>
       </div>
     </>

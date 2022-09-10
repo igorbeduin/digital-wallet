97773969b5ea06e7ca2977fa0116bcb780ad8bbf
Igor Beduin <beduinigor@gmail.com>
2022-09-10 16:24:48 -0300

    feat: initialize wallet db on app start

--- src/hooks/useInitialConfiguration.jsx
+++ src/hooks/useInitialConfiguration.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import users from "consts/users";
+import wallets from "consts/wallets";
 
 export function useInitialConfiguration() {
   useEffect(() => {
     localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("wallets", JSON.stringify(wallets));
   }, []);
 
   return null;

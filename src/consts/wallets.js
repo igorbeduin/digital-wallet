d08bf7619cf2f67e245cad3cd1e1c6458b306906
Igor Beduin <beduinigor@gmail.com>
2022-09-10 16:24:27 -0300

    feat: adds wallet initial configuration

---
+++ src/consts/wallets.js
@@ -0,0 +1,16 @@
+const wallets = {
+  admin: {
+    credit: 100000000,
+    currency: "R$",
+    cryptoCurrencies: {
+      bitcoins: {
+        credit: 0,
+      },
+      busd: {
+        credit: 0,
+      },
+    },
+  },
+};
+
+export default wallets;

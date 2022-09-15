6a45bc276b404c66e8c94f261644cc78b4ef4e7f
Igor Beduin <beduinigor@gmail.com>
2022-09-15 16:54:30 -0300

    fix: lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
-import { RoutesNavigator } from "navigation/RoutesNavigator";
+import { RoutesNavigator } from 'navigation/RoutesNavigator'
 
-import { AuthenticationContextProvider } from "contexts/AuthenticationContext";
-import { TransactionContextProvider } from "contexts/TransactionContext";
+import { AuthenticationContextProvider } from 'contexts/AuthenticationContext'
+import { TransactionContextProvider } from 'contexts/TransactionContext'
 
-import { useInitialConfiguration } from "hooks/useInitialConfiguration";
+import { useInitialConfiguration } from 'hooks/useInitialConfiguration'
 
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
-  useInitialConfiguration();
+function App () {
+  useInitialConfiguration()
 
   return (
     <BrowserRouter>
       <ToastContainer />
       <AuthenticationContextProvider>
-        <TransactionContextProvider>
-          <RoutesNavigator />
-        </TransactionContextProvider>
+      <TransactionContextProvider>
+        <RoutesNavigator />
+      </TransactionContextProvider>
       </AuthenticationContextProvider>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App

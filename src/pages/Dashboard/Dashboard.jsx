55dd711e49a230a3bca4043f067c1c6d553a9d40
Igor Beduin <beduinigor@gmail.com>
2022-09-14 23:44:01 -0300

    refact: adds Header component to Dashboard and improve component areas distribution

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import { Sidebar } from "components/Sidebar";
+import { Header } from "components/Header";
 
 export function Dashboard() {
   return (
     <>
-      <div className="bg-slate-50 h-screen flex flex-row justify-start items-center">
-        <nav className="w-80 h-full flex justify-center items-center p-2">
-          <div className="bg-slate-100 h-full w-full rounded-lg">
+      <div className="bg-slate-50 flex flex-row justify-start items-center">
+        <nav className="w-72 h-screen flex justify-center items-center">
+          <div className="h-full w-full">
             <Sidebar />
           </div>
         </nav>
-        <main className="w-full h-screen flex justify-center items-center p-2">
-          <div className="bg-slate-100 h-full w-full rounded-lg">
+        <div className="w-full h-screen mr-4 flex flex-col justify-start items-center">
+          <header className="min-h-fit h-16 w-full ">
+            <Header />
+          </header>
+          <div className="bg-slate-200  h-screen w-full p-6 rounded-lg">
             <Outlet />
           </div>
-        </main>
+        </div>
       </div>
     </>
   );

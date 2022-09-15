6ba88fcaf44ac87fa7e9d7ec36da8d978f4ec825
Igor Beduin <beduinigor@gmail.com>
2022-09-15 16:04:23 -0300

    refact: rename all files js -> ts and jsx -> tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { AuthenticationContextProvider } from "contexts/AuthenticationContext";
 import { RoutesNavigator } from "navigation/RoutesNavigator";
+
+import { AuthenticationContextProvider } from "contexts/AuthenticationContext";
+import { TransactionContextProvider } from "contexts/TransactionContext";
 
 import { useInitialConfiguration } from "hooks/useInitialConfiguration";
 
 import "react-toastify/dist/ReactToastify.css";
-import { TransactionContextProvider } from "contexts/TransactionContext";
 
 function App() {
   useInitialConfiguration();
--- src/navigation/RoutesNavigator.tsx
+++ src/navigation/RoutesNavigator.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { useAuthenticationContext } from "contexts/AuthenticationContext";
-import { Dashboard, SignUp, Login } from "pages";
-import { Home } from "pages/Home";
-import { Negotiate } from "pages/Negotiate";
-import { Transactions } from "pages/Transactions";
+import { useAuthenticationContext } from 'contexts/AuthenticationContext'
+import { Dashboard, SignUp, Login } from 'pages'
+import { Home } from 'pages/Home'
+import { Negotiate } from 'pages/Negotiate'
+import { Transactions } from 'pages/Transactions'
 
-export function RoutesNavigator() {
-  const { isSignedIn } = useAuthenticationContext();
+export function RoutesNavigator () {
+  const { isSignedIn } = useAuthenticationContext()
 
   return (
     <>
       <Routes>
-        {isSignedIn ? (
+        {isSignedIn
+          ? (
           <>
             <Route path="*" element={<Navigate to="/home" />} />
             <Route path="/" element={<Dashboard />}>
@@ -22,14 +23,15 @@ export function RoutesNavigator() {
               <Route path="transactions" element={<Transactions />} />
             </Route>
           </>
-        ) : (
+            )
+          : (
           <>
             <Route path="*" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
           </>
-        )}
+            )}
       </Routes>
     </>
-  );
+  )
 }

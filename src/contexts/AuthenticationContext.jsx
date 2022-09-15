a7e54fff67d9085000b6985b341e8d6038f1d640
Igor Beduin <beduinigor@gmail.com>
2022-09-14 23:17:44 -0300

    feat: adds loginUser, logout and updateUserWallet functions to context in order to improve isolated responsibilities

--- src/contexts/AuthenticationContext.jsx
+++ src/contexts/AuthenticationContext.jsx
@@ -1,3 +1,5 @@
+import { useWalletsDb } from "hooks/useWalletsDb";
+import { isEmpty } from "lodash";
 import React, { createContext, useContext, useState } from "react";
 
 const AuthenticationContext = createContext({
@@ -6,11 +8,34 @@ const AuthenticationContext = createContext({
 
 // eslint-disable-next-line react/prop-types
 export function AuthenticationContextProvider({ children }) {
-  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(true);
+  const [user, setUser] = useState({});
+  const { getUserWallet, setUserWallet } = useWalletsDb();
+
+  if (isSignedIn && isEmpty(user)) logout();
+
+  function loginUser(userId) {
+    const userWallet = getUserWallet(userId);
+    setUser({ username: userId, wallet: { ...userWallet } });
+    setIsSignedIn(true);
+  }
+
+  function logout() {
+    setUser({});
+    setIsSignedIn(false);
+  }
+
+  function updateUserWallet(newWallet) {
+    setUser((prev) => ({ ...prev, wallet: newWallet }));
+    setUserWallet({ userId: user.username, newWallet });
+  }
 
   const contextValue = {
     isSignedIn,
-    setIsSignedIn,
+    user,
+    loginUser,
+    logout,
+    updateUserWallet,
   };
 
   return (

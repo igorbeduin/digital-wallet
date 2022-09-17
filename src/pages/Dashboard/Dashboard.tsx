f9858b27622fc362ba64946b90e5293b081c5236
Igor Beduin <beduinigor@gmail.com>
2022-09-17 20:43:37 -0300

    refact: improve dashboard responsiveness

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -7,17 +7,17 @@ import { Header } from "components/Header";
 export function Dashboard() {
   return (
     <>
-      <div className="bg-slate-50 flex flex-row justify-start items-center">
-        <nav className="w-72 h-screen flex justify-center items-center">
+      <div className="bg-slate-50 min-h-screen flex flex-row justify-start items-stretch pr-6">
+        <nav className="w-72 min-h-screen flex justify-center items-start">
           <div className="h-full w-full">
             <Sidebar />
           </div>
         </nav>
-        <div className="w-full h-screen mr-4 flex flex-col justify-start items-center">
-          <header className="min-h-fit h-16 w-full ">
+        <div className="w-full min-h-screen flex flex-col justify-start items-center">
+          <header className="h-16 w-full ">
             <Header />
           </header>
-          <div className="bg-slate-200  h-screen w-full p-6 rounded-lg">
+          <div className="bg-slate-200 h-fit w-full p-6 rounded-lg">
             <Outlet />
           </div>
         </div>

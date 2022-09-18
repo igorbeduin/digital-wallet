5a699fa4e9f1d5415db383e25dc27b0567696980
Igor Beduin <beduinigor@gmail.com>
2022-09-17 21:01:19 -0300

    fix: improve background color size on Login and SignUp pages

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -16,7 +16,7 @@ export function Login() {
 
   return (
     <>
-      <div className="bg-slate-50 h-screen flex flex-row justify-center items-start py-32">
+      <div className="bg-slate-50 min-h-screen h-fit flex flex-row justify-center items-start py-32">
         <div className="h-fit-content w-8/12 max-w-screen-md flex flex-col justify-center items-center p-2">
           <div className="mb-2 flex flex-col justify-center items-center">
             <Logo size="big"/>
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -22,7 +22,7 @@ export function SignUp() {
 
   return (
     <>
-      <div className="bg-slate-50 h-screen flex flex-row justify-center items-start py-32">
+      <div className="bg-slate-50 min-h-screen h-fit flex flex-row justify-center items-start py-32">
         <div className="h-fit-content w-8/12 max-w-screen-md flex flex-col justify-center items-center p-2">
           <div className="mb-2 flex flex-col justify-center items-center">
             <Logo size="big" />
